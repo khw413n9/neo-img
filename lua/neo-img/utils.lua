2dccfc198420e5a1077c586e125dd997e75a7340
Meron Main PC <meronb1312@gmail.com>
2025-02-05 20:52:47 +0200

    fixing alot of issues

--- lua/neo-img/utils.lua
+++ lua/neo-img/utils.lua
@@ -2,31 +2,51 @@ local M = {}
 local config = require('neo-img.config').get()
 
 local echoraw = function(str)
-  vim.fn.chansend(vim.v.stderr, str)
+  local win_height         = vim.api.nvim_win_get_height(0)
+  local win_width          = vim.api.nvim_win_get_width(0)
+  local win_top            = vim.fn.line('w0')
+
+  local image_height_cells = math.floor(400 / 16)
+  local image_width_cells  = math.floor(600 / 8)
+  local row                = win_top + math.floor(win_height / 2) - math.floor(image_height_cells / 2)
+  local col                = math.floor(win_width / 2) - math.floor(image_width_cells / 2)
+
+  local move_cursor        = string.format("\27[%d;%dH", row, col)
+  local full_str           = "\27[s" .. move_cursor .. str .. "\27[u"
+
+  vim.fn.chansend(vim.v.stderr, full_str)
 end
 
 local get_extension = function(filename)
   return filename:match("^.+%.(.+)$")
 end
 
-local display_image = function(filepath)
-  local command
+local function build_command(filepath)
   if config.backend == "kitty" then
-    command = 'kitty +kitten icat "'
-  elseif config.backend == "chafa" then
-    command = 'chafa --align=center -s 60 --format=sixels "'
-  else
-    command = 'viu "'
+    return { "kitty", "+kitten", "icat", filepath }
+  elseif config.backend == "magick" then
+    return { "magick", filepath, "-resize", "600x400", "sixel:-" }
   end
-  command = command .. filepath .. '"'
+end
 
+local display_image = function(filepath)
   if vim.fn.filereadable(filepath) == 0 then
     vim.notify("File not found: " .. filepath, vim.log.levels.ERROR)
     return
   end
 
-  echoraw(vim.fn.system(command))
-  -- vim.fn.termopen(command .. filepath .. '"')
+  -- new buffer so gibbrish won't show and remove the echo
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_option(buf, "modifiable", false)
+  vim.api.nvim_set_current_buf(buf)
+
+  vim.api.nvim_command('mode')
+  local command = build_command(filepath)
+  vim.system(command, {}, function(obj)
+    vim.schedule(function()
+      echoraw(obj.stdout)
+    end)
+  end)
 end
 
 function M.setup_autocommands()
