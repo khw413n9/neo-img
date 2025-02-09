aedd488e1268014e72f6f7d67b5aa7b4d9b86013
Meron Main PC <meronb1312@gmail.com>
2025-02-09 13:20:39 +0200

    trying to embed ttyimg

--- lua/neo-img/config.lua
+++ lua/neo-img/config.lua
@@ -25,7 +25,55 @@ M.defaults = {
 
 local config = M.defaults
 
+
+local function build_ttyimg()
+  local go_dir = vim.fn.stdpath("config") .. "/ttyimg" -- Path to the submodule
+  config.binary_path = go_dir .. "/ttyimg"             -- Path to the binary
+
+  -- Run `go build` to compile the binary
+  local result = vim.system({
+    "go", "build", "-o", config.binary_path
+  }, { cwd = go_dir }):wait()
+
+  if result.code ~= 0 then
+    error("Failed to build ttyimg: " .. result.stderr)
+  end
+
+  print("Successfully built ttyimg!")
+end
+
+local function setup_plugin()
+  if pcall(require, "packer") then
+    -- Configure for packer.nvim
+    require("packer").startup(function(use)
+      use({
+        "skardyy/neo-img",
+        run = function()
+          build_ttyimg()
+        end,
+      })
+    end)
+  elseif pcall(require, "lazy") then
+    -- Configure for lazy.nvim
+    require("lazy").setup({
+      {
+        "skardyy/neo-img",
+        build = function()
+          build_ttyimg()
+        end,
+      },
+    })
+  else
+    -- No plugin manager detected
+    vim.notify(
+      "No plugin manager detected! Please run `go install github.com/Skardyy/ttyimg@latest`.",
+      vim.log.levels.WARN
+    )
+  end
+end
+
 function M.setup(opts)
+  setup_plugin()
   -- Normalize size options before merging
   if opts and opts.size then
     for k, v in pairs(opts.size) do
